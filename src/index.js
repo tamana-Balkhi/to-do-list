582b091a7a8b8bc8d2156f7901500f9b8ffdf6ae
tamana balkhi <tamana.balkhi1377@gmail.com>
2022-08-12 21:23:58 +0430

    remove linters errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import _ from 'lodash';
+
 import './style.css';
+
 import creatTasks from './modules/layout.js';
+
 creatTasks();
--- src/modules/arrlists.js
+++ src/modules/arrlists.js
@@ -4,21 +4,25 @@ const arrLists = [
     description: 'Complete daily Task',
     completed: true,
   },
+
   {
     id: 2,
     description: 'Complete daily project',
     completed: true,
   },
+
   {
     id: 3,
     description: 'Complete weekly restrospectives',
     completed: true,
   },
+
   {
     id: 4,
     description: 'Complete monthly capestone',
     completed: true,
   },
+
 ];
 
 export default arrLists;
