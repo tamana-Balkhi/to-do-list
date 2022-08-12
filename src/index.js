73ba634f8977ebf927e08c896b09157f2f99ded9
tamana balkhi <tamana.balkhi1377@gmail.com>
2022-08-12 21:08:14 +0430

    remove unnecccery code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,4 @@
 import _ from 'lodash';
 import './style.css';
-
-/*function component() {
-  const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());*/
-
 import creatTasks from './modules/layout.js';
 creatTasks();
