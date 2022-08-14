2d997346e7fca5677abe77810a1ce446a12f7572
tamana balkhi <tamana.balkhi1377@gmail.com>
2022-08-14 05:41:30 +0430

    import storage and add new functionalty

--- src/index.js
+++ src/index.js
@@ -1,5 +1,58 @@
 import './style.css';
+import ToDOList from './modules/layout.js';
+import CreateList from './modules/arrlists.js';
+import Storage from './modules/localStorage.js';
 
-import creatTasks from './modules/layout.js';
+document.addEventListener('DOMContentLoaded', ToDOList.displayToDo);
 
-creatTasks();
+document.getElementById('cleartask').addEventListener('click', () => {
+  Storage.removeCompleted();
+});
+
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const todoL = Storage.getToDo();
+  const toDoInput = document.getElementById('todo-input').value;
+  const id = todoL.length + 1;
+  const completed = false;
+
+  const todo = new CreateList(toDoInput, id, completed);
+
+
+  ToDOList.addLList(todo);
+
+
+  Storage.addL(todo);
+
+
+  ToDOList.clearField();
+});
+
+
+document.getElementById('cleartask').addEventListener('click', () => { });
+
+
+document.getElementById('to-do-container').addEventListener('click', (e) => {
+
+  Storage.editInput(
+    e.target.parentElement.parentElement.children[4].textContent,
+    e.target.parentElement,
+    e.target.parentElement.parentElement,
+    e.target.parentElement.parentElement.children[2].children[0],
+  );
+
+  ToDOList.deleteTodo(e.target);
+
+  if (e.target.classList.contains('check')) {
+    Storage.checkboxCompleted(e.target.parentElement.parentElement.children[4]);
+    e.target.parentElement.parentElement.children[2].children[0].classList.toggle(
+      'strike-through',
+    );
+
+  }
+
+  Storage.remove(
+    e.target.parentElement.previousElementSibling.previousElementSibling
+      .textContent,
+  );
+});
